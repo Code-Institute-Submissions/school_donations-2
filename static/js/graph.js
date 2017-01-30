36112cc443fb721125bfeb49a4ae0fcb74a5c4d2
Irene <irene.g5555@gmail.com>
2017-01-30 22:20:55 +0000

    Create index.html as the main page to show the graphs. Create graphs.js with code to get the data and create and render the graphs.

---
+++ static/js/graph.js
@@ -0,0 +1,141 @@
+/**
+ * Created by Irene on 30/01/2017.
+ */
+
+//queue() function utilises the queue library for asynchronous loading.
+//Useful to get data from multiple API's for a single analysis.
+//This function processes the data and inserts it into the apiData Variable
+queue()
+    .defer(d3.json, "/donorsUS/projects")
+    .await(makeGraphs);
+
+function makeGraphs(error, projectsJson){
+
+    //Clean projectJson data
+    var donorsUSProjects = projectsJson; //Pass the data inside the projectsJson variable into our dataSet variable
+    var dataFormat = d3.time.format("%Y-%m-%d %H:%M:%S"); // Parse the date data type to suit our charting needs
+    donorsUSProjects.forEach(function(d){
+        d["date_posted"] = dateFormat.parse(d["date_posted"]); //Set the data type as number using the + operator
+        d["date_posted"].setDate(1); // we set all projects date days to 1.
+        // All projects from the same month will have the same dateime value
+        d["total_donations"] = +d["total_donations"]
+    });
+}
+
+    //Ingesting the data into a crossfilter instance and creating dimensions based on the crossfilter instance
+    //Crossfilter acts as a two way data binding pipeline.
+    //Whenever you make a selection on the data, it is automatically applied to other charts,
+    // as well enabling our drill down functionality.
+
+    //Create a Crossfilter instance
+    var ndx = crossfilter(donorsUSProjects);
+
+    //Define Dimensions
+    var dateDim = ndx.dimension(function(d){
+        return d["date_posted"];
+    });
+    var resourceTypeDim = ndx.dimension(function(d){
+        return d["resource_type"];
+    });
+    var povertyLevelDim = ndx.dimension(function(d){
+        return d["poverty_level"];
+    });
+    var stateDim = ndx.dimension(function (d) {
+        return d["school_state"];
+    });
+    var totalDonationsDim = ndx.dimension(function (d) {
+        return d["total_donations"];
+    });
+    var fundingStatus = ndx.dimension(function(d){
+        return d["funding_status"];
+    });
+
+    //Calculate metrics and groups for grouping and counting our data
+    var numProjectsByDate = dateDim.group();
+    var numProjectsByResourceType = resourceTypeDim.group();
+    var numProjectsByPovertyLevel = povertyLevelDim.group();
+    var numProjectsByFundingStatus = fundingStatus.group();
+    var totalDonationsByState = stateDim.group().reduceSum(function (d){
+        return d["total_donations"];
+    });
+    var stateGroup = stateDim.group();
+
+    var all=ndx.groupAll();
+    var totalDonations = ndx.groupAll().reduceSum(function (d){
+        return d["total_donations"];
+    });
+
+    var max_state = totalDonationsByState.top(1)[0].value;
+
+    //Define values (to be used in charts)
+    var minDate = dateDim.bottom(1)[0]["date_posted"];
+    var maxDate = dateDim/top(1)[0]["date_posted"];
+
+    //We define the chart types objects using DC.js library.
+    //We also bind the charts to the div ID's in index.html
+    //Charts
+    var timeChart = dc.barChart("#time-chart");
+    var resourceTypeChart = dc.rowChart("#resource-type-row-chart");
+    var povertyLevelChart = dc.rowChart("#poverty-level-row-chart");
+    var numberProjectsND = dc.numberDisplay("#number-projects-nd");
+    var totalDonationsND = dc.numberDisplay("#total-donations-nd");
+    var fundingStatusChart = dc.pieChart("#funding-chart");
+
+
+    //We assign properties and values to our charts.
+    //We also include a select manu to choose between any of all US states for a particlar date
+    selectField = dc.selectMenu('#menu-select')
+        .dimension(stateDim)
+        .group(stateGroup);
+
+    numberProjectsND
+        .formatNumber(d3.format("d"))
+        .valueAccessor(function(d){
+            return d;
+        })
+        .group(all);
+
+    totalDonationsND
+        .formatNumber(d3.format("d"))
+        .valueAccessor(function (d){
+            return d;
+        })
+        .group(totalDonations)
+        .formatNumber(d3.format(".3s"));
+
+    timeChart
+        .width(800)
+        .height(200)
+        .margins({top:10, right:50, bottom:30, left:50})
+        .dimension(dateDim)
+        .group(numProjectsByDate)
+        .transitionDuration(500)
+        .x(d3.time.scale().domain([minDate, maxDate]))
+        .elasticY(true)
+        .xAxisLabel("Year")
+        .yAxis().ticks(4);
+
+    resourceTypeChart
+        .width(300)
+        .height(250)
+        .dimension(resourceTypeDim)
+        .group(numProjectsByResourceType)
+        .xAxis().ticks(4);
+
+    povertyLevelChart
+        .width(300)
+        .height(250)
+        .dimension(povertyLevelDim)
+        .group(numProjectsByPovertyLevel)
+        .xAxis().ticks(4);
+
+    fundingStatusChart
+        .height(220)
+        .radius(90)
+        .innerRadius(40)
+        .transitionDuration(1500)
+        .dimension(fundingStatus)
+        .group(numProjectsByFundingStatus);
+
+    dc.renderAll();
+}
